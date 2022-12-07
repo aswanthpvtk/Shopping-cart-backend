87f47b8486c6d3a9a224b13b73e46933d84b7ec9
Aswanth P <Aswanth@NTP-LAP-734.chn.nestgroup.net>
2022-12-07 20:13:34 +0530

    cart

---
+++ src/test/java/com/nest/shopping_cart_backend/ShoppingCartBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nest.shopping_cart_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
